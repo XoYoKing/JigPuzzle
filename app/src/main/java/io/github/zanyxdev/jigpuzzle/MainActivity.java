090f9e186cc1ced512e36ecc7a15bb2d42418373
ZanyXDev <zanyxdev@gmail.com>
2017-10-17 05:58:04 +0300

    screate View from xml layout and add custom surfaceview to FrameLayout

--- app/src/main/java/io/github/zanyxdev/jigpuzzle/MainActivity.java
+++ app/src/main/java/io/github/zanyxdev/jigpuzzle/MainActivity.java
@@ -2,6 +2,11 @@ package io.github.zanyxdev.jigpuzzle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private GamePanel gamePanel;
@@ -9,9 +14,33 @@ private GamePanel gamePanel;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     gamePanel = new GamePanel(this);
-    setContentView(gamePanel);
+    setContentView(R.layout.activity_main);
+
+
+    FrameLayout frm=(FrameLayout)findViewById(R.id.frameLayout);
+    frm.addView(gamePanel);
+
+    Button btnC=(Button)findViewById(R.id.buttonColor);
+
+    btnC.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        switch (v.getId()) {
+          case R.id.buttonColor:
+            Toast.makeText(getApplicationContext(), "Color", Toast.LENGTH_LONG).show();
+            gamePanel.colorNew();
+
+            break;
+
+          default:
+            break;
+        }
+      }
+    });
   }
+
   @Override
   protected void onResume() {
     super.onResume();
